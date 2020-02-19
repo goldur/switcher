708582c3513b4725ee84853371d153c2bf0ad237
Wolfgang Binder <w.binder@knware.de>
2020-02-19 15:56:07 +0100

    reverted initialBoards to normal 5x5 boards

--- src/initialBoards.js
+++ src/initialBoards.js
@@ -1,7 +1,6 @@
 import createBoard from './createBoard'
 
 const initialBoards = [
-  createBoard(6, 5, '000000111001010011100000011111'),
   createBoard(5, 5, '0000001110010100111000000'),
   createBoard(5, 5, '1000101010001000101010001'),
   createBoard(5, 5, '0010001010101010101000100'),
